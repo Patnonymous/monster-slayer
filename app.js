86c100882549d9388aae73de0e7fde92c58cc5b6
Patrick Williamson <000800470@mohawkcollege.ca>
2020-09-14 14:02:37 -0400

    added attack functions

--- app.js
+++ app.js
@@ -10,6 +10,48 @@ new Vue ({
             this.gameRunning = true;
             this.playerHP = 100;
             this.monsterHP = 100;
+        },
+        attack: function() {
+            this.monsterHP -= this.calculateDamage(2, 10);
+            if (this.checkResult()) {
+                return;
+            }
+
+            this.playerHP -= this.calculateDamage(5, 15);
+            this.checkResult();
+        },
+        specialAttack: function() {
+
+        },
+        heal: function() {
+
+        },
+        giveUp: function() {
+
+        },
+        calculateDamage: function(minDamage, maxDamage) {
+            return Math.max(Math.floor(Math.random() * maxDamage) + 1, minDamage);
+        },
+        checkResult: function() {
+            if (this.monsterHP <= 0) {
+                if(confirm('Winner! Start a new game?')) {
+                    this.startGame();
+                }
+                else {
+                    this.gameRunning = false;
+                }
+                return true;
+            }
+            else if (this.playerHP <= 0) {
+                if(confirm('Loser! Start a new game?')) {
+                    this.startGame();
+                }
+                else {
+                    this.gameRunning = false;
+                }
+                return true;
+            }
+            return false;
         }
     }
 
